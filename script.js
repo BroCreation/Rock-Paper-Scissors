d9e40a0ed5c400754ae0fbf381fb802bd47c759b
Abdul Wasay <abdulwasaychoa@gmail.com>
2025-07-24 13:15:04 -0700

    Add the js refactor code to suit DOM instead of console. Redo player unit selection and game window proper result display with scores and winner announcement.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function getRandomNumBetween(min, max) {
 
 function getComputerChoice() {
     const choice = getRandomNumBetween(0, 2);
+    // changes image
+    const computerImage =  document.querySelector('.right-image img')
+    computerImage.src = `images/${choice}.webp`;
     switch(choice) {
         case 0:
             return "scout";
@@ -16,30 +19,63 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    const choice = prompt("Which unit will you choose? Scout - Skirm - Spear").toLowerCase();
+function getHumanChoice(e) {
+    const choice = e.target.value;
+    const humanImage =  document.querySelector('.left-image img')
+    switch(choice) {
+        case "scout":
+            humanImage.src = `images/0.webp`
+            break
+        case "skirm":
+            humanImage.src = `images/1.webp`
+            break
+        case "spear":
+            humanImage.src = `images/2.webp`
+            break
+    }
     return choice;
 }
 
+
 function playGame() {
+    const scoresElem = document.querySelector('.scores')
+    const message = document.querySelector('.message')
+    const buttons = document.querySelectorAll('button')
+    const div = document.querySelector('div.box')
+    const outcome = document.createElement('p')
     let humanScore = 0, computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("Draw!", `Both chose ${humanChoice}.`)
-        } else if (
-            (humanChoice === "scout" && computerChoice === "skirm") ||
-            (humanChoice === "skirm" && computerChoice === "spear") ||
-            (humanChoice === "spear" && computerChoice === "scout")) {
-                console.log(`You Win! ${humanChoice} beats ${computerChoice}`)
-                humanScore++
-        } else if (
-            (computerChoice === "scout" && humanChoice === "skirm") ||
-            (computerChoice === "skirm" && humanChoice === "spear") ||
-            (computerChoice === "spear" && humanChoice === "scout")) {
-                console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-                computerScore++
+    function playRound(event) {
+        if (humanScore < 5 && computerScore < 5) {
+            let humanChoice = getHumanChoice(event)
+            let computerChoice = getComputerChoice()
+            if (humanChoice === computerChoice) {
+                message.textContent = `Draw! Both chose ${humanChoice}.`
+            } else if (
+                (humanChoice === "scout" && computerChoice === "skirm") ||
+                (humanChoice === "skirm" && computerChoice === "spear") ||
+                (humanChoice === "spear" && computerChoice === "scout")) {
+                    message.textContent = `You Win! ${humanChoice} beats ${computerChoice}`
+                    humanScore++
+            } else if (
+                (computerChoice === "scout" && humanChoice === "skirm") ||
+                (computerChoice === "skirm" && humanChoice === "spear") ||
+                (computerChoice === "spear" && humanChoice === "scout")) {
+                    message.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`
+                    computerScore++
+            }
+            scoresElem.textContent = `Human Score: ${humanScore} Computer Score: ${computerScore}`
+        } 
+        if (humanScore >= 5) {
+            outcome.textContent = "Congrats! You won the Aoe2 Unit War against AI"
+        } else if (computerScore >= 5){
+            outcome.textContent = "Alas! You lost this match against AI"
         }
+        div.appendChild(outcome)
     }
+
+    buttons.forEach(button => {
+        button.addEventListener('click', playRound)
+    })
 }
 
 playGame();
